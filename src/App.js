078c26f85a56a0ad7e1029ebadc84be35226f078
Elodie-n <94834435+Elodie-n@users.noreply.github.com>
2022-02-27 19:09:39 +0100

    Search engine added + dictionary api integration + rendered the word meanings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <div className="App-title shadow">Dictionary</div>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/Elodie-n/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open-source code
+            </a>{" "}
+            by Elodie Nerjat
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
